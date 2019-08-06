a791bda571fca79a3c5a4f5bdbf59f63f33ed435
vspolimenov <vaskospolimenov@gmail.com>
2019-08-06 23:07:44 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
 import { Component } from '@angular/core';
-
+import {FormBuilder, FormGroup} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bobeto';
+
+  options: FormGroup;
+  title = 'airwolfyProject';
+  isMenu:boolean;
+
+  constructor(fb: FormBuilder) {
+    this.options = fb.group({
+      'fixed': false,
+      'top': 0,
+      'bottom': 0,
+    });
+  }
+  ngOnInit(){
+    this.isMenu = false;
+}
+  goToCrew(){
+    document.body.scrollTop = 700;
+    document.documentElement.scrollTop = 700;
+  }
+
+  changeOpacity(value:number, id:string){
+    let i = 1;
+    console.log(value-i*0.1 + "id: " + id);
+    while(i < 10) {
+      let change = value-i*0.1;
+      console.log(change);
+      document.getElementById(id).style.opacity = (change).toString();
+    i++;
+    }
+   }
+  
+  mouseEnter(div : string){
+  //this.changeOpacity(1,div);
+    console.log("Enter")
+ }
+
+ mouseLeave(div : string){
+ }
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+
+  public modalImg:string;
+  public videoModal:string;
+ 
+  
+  // When the user clicks on <span> (x), close the modal
+
+ 
+ onClickImg(image:string, id){
+  var modal = document.getElementById(id);
+    modal.style.display = "block";
+    this.modalImg = image;
+  }
+
+  hideModal(id){
+    var modal = document.getElementById(id);
+    modal.style.display = "none";
+  }
+ 
+clickMenu(){
+if(this.isMenu) {
+  this.isMenu = false;
+}else {
+  this.isMenu = true;
+}
+}
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
