40dc6b5b6118a0d82144339b65f59c1c8d0862ca
vaspol <vasilen.polimenov@nevexis.com>
2020-05-02 22:42:33 +0300

    v2.0.8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,4 +40,13 @@ saveWish(){
     i++;
     }
    }
+
+   openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+ closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
 }
